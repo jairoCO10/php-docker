99f240baccee8d00ddd37f6d19e93eaa76d6721e
santiago padilla arcia <santiagopadilla117@gmail.com>
2023-04-29 04:43:52 -0500

    formated head.php

--- www/taller-01/views/head/head.php
+++ www/taller-01/views/head/head.php
@@ -1,28 +1,20 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Nav</title>
-  </head>
-  <body>
+</head>
 
-<?php
-    // require_once('./api/settings/Dbconection.php');
-    // require_once('./api/models/UserModel.php');
-    // require_once('./api/controller/UserController.php');
-    // require_once('./api/models/GeneroModel.php');
-    // require_once('./api/controller/GeneroController.php');
-    // require_once('./api/models/ProgramaModel.php');
-    // require_once('./api/controller/ProgramaController.php');
-?> 
+<body>
 
     <nav class="bg-indigo-500">
         <div class="container py-2 mx-auto px-10 flex flex-wrap items-center justify-between">
-            
+
             <!-- BEGIN logo -->
-            <div  class="flex items-center">
+            <div class="flex items-center">
                 <img src="./public/imgs/logou2.png" class="h-14" alt="img-logo">
                 <h2 class="text-white">Universidad de Córdoba</h2>
             </div>
@@ -30,9 +22,13 @@
 
 
             <!-- ENG Botón responsive -->
-            <button id="bntResponsive" class="p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-white" data-collapse-toggle="mobile-menu" type="button"  aria-controls="mobile-menu" aria-expanded="false">
-                <svg id="iconOpen" class="text-white w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                <svg id="iconClose" class="text-white hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <button id="bntResponsive" class="p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-white" data-collapse-toggle="mobile-menu" type="button" aria-controls="mobile-menu" aria-expanded="false">
+                <svg id="iconOpen" class="text-white w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                </svg>
+                <svg id="iconClose" class="text-white hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                </svg>
             </button>
             <!-- END Botón responsive -->
 
@@ -56,10 +52,10 @@
                         <a href="#" class="block py-2 pr-4 pl-3 text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Contact</a>
                     </li>
                 </ul>
-              </div>
+            </div>
             <!-- END items adicionales -->
-            
-            
+
+
         </div>
     </nav>
 
@@ -96,5 +92,6 @@
             </ul>
         </div>
     </nav>
-    </body>
+</body>
+
 </html>
